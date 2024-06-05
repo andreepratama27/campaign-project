3e44fdb0b668c836d29fac4af46ed5a3799327ee
andre7131041 <andre@primaku.com>
2024-06-05 15:33:43 +0700

    feat: update logo

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,12 +1,13 @@
+import Image from "next/image";
 import CategoryFilter from "../category-filter";
 
 export default function Navbar() {
-  return (
-    <nav className="flex h-14 items-center justify-between px-4 md:px-0">
-      <div className="logo-wrapper">
-        <p className="font-normal">Kitabisa</p>
-      </div>
-      <CategoryFilter />
-    </nav>
-  );
+	return (
+		<nav className="flex h-14 items-center justify-between px-4 md:px-0">
+			<div className="logo-wrapper">
+				<Image src="/logo.png" width={100} height={80} alt="kitabisa-logo" />
+			</div>
+			<CategoryFilter />
+		</nav>
+	);
 }
